91ad3a7102bef6441e222619df68b22d6e9a06c3
Dexter Lewis <dexter.lewis@gmail.com>
2019-05-15 22:25:20 -0400

    Updated process.env files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const server = require('./api/server.js');
+const PORT = process.env.PORT || 4000;
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on ${PORT} ***\n`);
 });
